318c8feb90032e6befc4edd30a51dffb3c1cbd05
Julio <JulioToboso@gmail.com>
2019-05-14 14:15:48 +0200

    Add files via upload

---
+++ test.adb
@@ -0,0 +1,31 @@
+--gnatmake test.adb && test
+with Ada.Text_IO;
+with Ranges;
+with JSON;
+
+procedure Test is
+  use Ranges;
+  use JSON;
+
+  N : Interval := (From => 10, To => 100);
+  M : Interval := (From => 1, To => 10);
+begin
+   Put (N);
+
+   Put_Line (M);
+   Put_Line(N+M);
+   Put_Line(-M);
+   Put_Line(N-M);
+   Put_Line(N*M);
+   Put_Line(N/M);
+   Put_Line(N and M);
+   Put_Line(N or M);
+
+
+
+
+end Test;
+
+
+--Compilation:
+-- On Windows: gnatmake test.adb && test.exe
