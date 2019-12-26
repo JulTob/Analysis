4221b2e2b1a96379192c5c8e4a663d707b4438d7
Julio <JulioToboso@gmail.com>
2019-12-26 15:18:56 +0100

    Update signals.ads

--- E.Signals_and_Systems/signals.ads
+++ E.Signals_and_Systems/signals.ads
@@ -5,15 +5,22 @@ package Signals is
   procedure Put(S: Signal);
   function "+"(S1: Signal; S2: Signal) return Signal;
   function "-"(S1: Signal; S2: Signal) return Signal;
-  function "*"(S1: Signal; S2: Signal) return Signal; -- Linear w-Selector, dot product
-  function "*"(v: Value; S: Signal) return Signal; --Linear scale
-  function "**"(S1: Signal; S2: Signal) return Signal; --Convolution
+  function "*"(S1: Signal; S2: Signal) return Signal; 
+     --   Linear w-Selector, dot product
+  function "*"(v: Value; S: Signal) return Signal; 
+     --   Linear scaling
+  function "**"(S1: Signal; S2: Signal) return Signal; 
+     --   Convolution
   function modtime(from,to: Integer; time_delay: Integer := 0) return Signal;
-  function saw(from,to: Integer; time_delay: Integer := 0) return Signal; --AKA modtime signal /12
+  function saw(from,to: Integer; time_delay: Integer := 0) return Signal; 
+     --   AKA modtime signal /12
 
-  function delta_signal(from: integer := -1; to: Integer := 1; center_at: Integer := 0)
+
+  function delta_signal 
+    (from: integer := -1; to: Integer := 1; center_at: Integer := 0)
     return Signal ;
-  function step(from: integer := -1; to: Integer := 1; center_at: Integer := 0)
+  function step
+      (from: integer := -1; to: Integer := 1; center_at: Integer := 0)
       return Signal ;
   function add(S: Signal) return Float;
 
