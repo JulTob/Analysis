59a0f63d48b3af18f51b0e65b4a447253c14f576
Julio <JulioToboso@gmail.com>
2020-04-12 16:29:07 +0200

    Update coordenadas.ads

--- coordenadas/coordenadas.ads
+++ coordenadas/coordenadas.ads
@@ -1,1 +1,22 @@
+package Coordenadas is
 
+    subtype Number is digits 10;
+
+
+    type Position is record
+       X : Number;
+       Y : Number;
+       Z : Number;
+      end record;
+
+    function "+"(Left,Right : Position) return Position;
+    function "-"(Left,Right : Position) return Position;
+
+    function Modulus (Point: Position)
+      return Number;
+
+    function Distance (Point1: Position, Point2: Position)
+      return Number;
+
+
+ end Coordenadas;
